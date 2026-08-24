02941cd1f042559abcd16d6b8c2e1621ad8db91f
Ibrahim Moosa <imoosa53@gmail.com>
2026-08-24 11:24:47 +0530

    build changes

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,9 +1,3 @@
-plugins {
-    id("com.android.application") version "8.7.2" apply false
-    id("com.android.library") version "8.7.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false  // ADD THIS
-}
-
 allprojects {
     repositories {
         google()
